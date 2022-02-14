329701d4474dc42a88e304d7b6efe2f2ad0e830c
Oguzhan <ornekoguzhan@gmail.com>
2022-02-14 23:06:41 +0300

    Initial commit

---
+++ app/src/main/java/com/ta/yemekatolyesi/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ta.yemekatolyesi.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ta.yemekatolyesi.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
